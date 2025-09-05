3800d5ec9f6c25d933ae3bb0fbfc528db977d03a
Rajat Dhakad <rajatdhakad5@gmail.com>
2025-09-06 01:06:27 +0530

    fourth cmt

--- src/main/java/com/rays/Rajat.java
+++ src/main/java/com/rays/Rajat.java
@@ -4,7 +4,7 @@ public class Rajat {
 
 	public static void main(String[] args) {
 	
-System.out.println("My Name IS Ramesh");
+System.out.println("My Name IS Suresh");
 		System.out.println("My Name IS Dinesh");
 	}
 
